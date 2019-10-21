2e143c9bc64794e8f5cbb92300d10ccca04198f5
EthanFe <sneakcow@gmail.com>
2019-10-21 16:59:32 -0500

    clicking is now Powerful

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ const style = {
 }
 
 const conversations = require('./conversations.json');
-console.log(conversations)
+let timeout = null;
 
 function App() {
   const [conversationIndex, setConversation] = useState(0)
   useEffect(() => {
-    setTimeout(() => {
+    timeout = setTimeout(() => {
       // if (message < messages.length) {
         setConversation(conversationIndex < conversations.length - 1 ? conversationIndex + 1 : 0)
       // }
     }, 3000)
   }, [ conversationIndex ]);
 
+  const cycleMessage = () => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    setConversation(conversationIndex < conversations.length - 1 ? conversationIndex + 1 : 0)
+  }
+
   return (
-    <div style={style}>
+    <div style={style} onClick={cycleMessage}>
       <Wrapper conversation={conversations[conversationIndex]} conversationKey={conversationIndex}/>
     </div>
   );
