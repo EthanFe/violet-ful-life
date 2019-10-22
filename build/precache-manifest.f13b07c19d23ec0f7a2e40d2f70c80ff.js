a815f337beb93c8b038215c4f37ffc4db0977c95
EthanFe <sneakcow@gmail.com>
2019-10-21 21:34:10 -0500

    iunno.

---
+++ build/precache-manifest.f13b07c19d23ec0f7a2e40d2f70c80ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525dc36baa0cdc6c202f2966724fa4e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "577f62d3204fc07a8392",
+    "url": "/static/css/main.19dea7cb.chunk.css"
+  },
+  {
+    "revision": "e4d403785ea508d7017e",
+    "url": "/static/js/2.5e523457.chunk.js"
+  },
+  {
+    "revision": "577f62d3204fc07a8392",
+    "url": "/static/js/main.db0c6ace.chunk.js"
+  },
+  {
+    "revision": "810c86df38f8221cd32c",
+    "url": "/static/js/runtime-main.790e7e5d.js"
+  }
+]);
